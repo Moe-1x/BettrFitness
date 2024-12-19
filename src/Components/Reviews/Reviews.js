006f80f98d81059d2d48cd302970cff96f55a687
Moe-1x <midoalaa12345@gmail.com>
2024-12-19 16:09:35 +0000

    modified:   src/Components/Reviews/Reviews.js

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -11,7 +11,7 @@ const Reviews = () => {
         <div className="space-y-8">
           <div className="relative  w-full max-[970px]:w-[98%] left-1/2 -translate-x-1/2 aspect-w-16 aspect-h-9">
             <iframe
-              className="max-[970px]:w-[95%] relative left-1/2  w-full -translate-x-1/2 h-full rounded-[20px] max-[1024px]:min-h-[700px]  min-h-[275px]"
+              className="max-[970px]:w-[95%] relative left-1/2  w-full -translate-x-1/2 h-full rounded-[20px] max-[1023px]:min-h-[700px]  min-h-[275px]"
               src="https://www.youtube.com/embed/example1"
               title="Video 1"
               frameBorder="0"
@@ -24,7 +24,7 @@ const Reviews = () => {
           </div>
           <div className="relative max-[970px]:w-[98%]  w-full left-1/2 -translate-x-1/2 aspect-w-16 aspect-h-9">
             <iframe
-              className="max-[970px]:w-[95%]  w-full relative left-1/2 -translate-x-1/2  h-full rounded-[20px] max-[1024px]:min-h-[700px]  min-h-[275px]"
+              className="max-[970px]:w-[95%]  w-full relative left-1/2 -translate-x-1/2  h-full rounded-[20px] max-[1023px]:min-h-[700px]  min-h-[275px]"
               src="https://www.youtube.com/embed/example2"
               title="Video 2"
               frameBorder="0"
@@ -39,14 +39,14 @@ const Reviews = () => {
 
         <div className="col-span-1 lg:col-span-2 left-1/2 -translate-x-1/2 w-full max-[970px]:w-[98%] aspect-w-16 aspect-h-9 relative">
           <iframe
-            className="max-[970px]:w-[95%]  w-full relative left-1/2 -translate-x-1/2  h-full max-[1024px]:min-h-[700px]  rounded-[20px] "
+            className="max-[970px]:w-[95%]  w-full relative left-1/2 -translate-x-1/2  h-full max-[1023px]:min-h-[700px]  rounded-[20px] "
             src="https://www.youtube.com/embed/example3"
             title="Video 3"
             frameBorder="0"
             allow="accelerometer; autoplay;  clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
-          <div className="absolute bottom-2 pl-4 pb-4 left-2 max-[1024px]:text-[16px] text-white text-[24px] font-sans font-medium">
+          <div className="absolute bottom-2 pl-4 pb-4 left-2 max-[1023px]:text-[16px] text-white text-[24px] font-sans font-medium">
             Ahmed Farag - US{" "}
           </div>
         </div>
